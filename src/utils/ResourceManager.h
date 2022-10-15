67766d18e5f3478032485417e29d91234b401f1f
Jakub Šilhavý <jakub.sil@seznam.cz>
2022-10-15 16:24:47 +0200

    backup

--- src/utils/ResourceManager.h
+++ src/utils/ResourceManager.h
@@ -12,31 +12,31 @@
 
 namespace kiv_ppr
 {
-	class CResource_Manager
-	{
-	public:
-		void Set_Run_Type(CArg_Parser::NRun_Type run_type);
-		void Find_Available_GPUs(const std::unordered_set<std::string>& listed_devices);
+    class CResource_Manager
+    {
+    public:
+	    void Set_Run_Type(CArg_Parser::NRun_Type run_type);
+	    void Find_Available_GPUs(const std::unordered_set<std::string>& listed_devices);
 
-	private:
-		void Inform_User_About_Found_Devs(const std::unordered_set<std::string>& found_devices,
-										  const std::unordered_set<std::string>& listed_devices);
+    private:
+        void Inform_User_About_Found_Devs(const std::unordered_set<std::string>& found_devices,
+                                          const std::unordered_set<std::string>& listed_devices);
 
-	private:
-		enum class NDevice_Status : uint8_t
-		{
-			AVAILABLE,
-			TAKEN
-		};
+    private:
+        enum class NDevice_Status : uint8_t
+        {
+            AVAILABLE,
+            TAKEN
+        };
 
-		struct TRecource
-		{
-			NDevice_Status status;
-			cl::Device device;
-		};
+        struct TRecource
+        {
+            NDevice_Status status;
+            cl::Device device;
+        };
 
-	private:
-		std::vector<TRecource> m_gpu_devices;
-		CArg_Parser::NRun_Type m_run_type{};
-	};
+    private:
+        std::vector<TRecource> m_gpu_devices;
+        CArg_Parser::NRun_Type m_run_type{};
+    };
 }
